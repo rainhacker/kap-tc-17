cd50770f749cef376ae7ff0229b580ca7feb27b0
Parth <parth.solanki@gmail.com>
2017-05-13 19:44:59 -0400

    rest api call to delivery.com

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var delivery = require('./routes/delivery');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/delivery', delivery);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -49,6 +51,7 @@ app.set('port', process.env.PORT || 3000)
 
 app.listen(app.get('port'), function() {
     console.log("Listening on " + app.get('port'));
+
 })
 
 
---
+++ routes/delivery.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+var Client = require('node-rest-client').Client;
+var request = require('request');
+
+var client = new Client();
+ 
+var url = 'https://www.delivery.com/api/data/search?search_type=alcohol&address=1006+Avenue+of+the+Americas,10018&order_time=ASAP&order_type=delivery&client_id=YzczMWQzMGYzNDg4NjM5YjVmYWJmMzhiNDU0MjA5YzI5';
+
+	router.get('/', function(req, res, next) {
+
+		request.get(url, function (error, response, body) {
+	    if (!error && response.statusCode == 200) {
+	        var respp = JSON.parse(body);
+	        console.log(respp.data.categories);
+	        console.log('-------------');
+	        console.log(body);
+	        res.render('index', { title:  body});
+	     }
+	});
+});
+
+module.exports = router;
+
+
+
+
+
+
