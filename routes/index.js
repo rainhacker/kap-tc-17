f53a16cd0eacb53fd8abc1f3de2e9b7c548527b2
trequartista_10 <mailkuldeepm@gmail.com>
2017-05-13 21:24:12 -0400

    Merge remote-tracking branch 'remotes/origin/master'
    # Conflicts:
    #	app.js

--- routes/index.js
+++ routes/index.js
@@ -14,4 +14,45 @@ router.get('/audio', function (req,res,next) {
     res.render('voiceip');
 })
 
+router.get('/uploadFile', function (req,res,next) {
+    console.log(res)
+})
+
+router.post('/uploadFile', function (req,res,next) {
+    uploadFile(req,res)
+})
+
+
+function uploadFile(request, response) {
+// parse a file upload
+    var mime = require('mime');
+    var formidable = require('formidable');
+    var util = require('util');
+
+    var form = new formidable.IncomingForm();
+
+    var dir = !!process.platform.match(/^win/) ? '\\uploads\\' : '/uploads/';
+
+    form.uploadDir = __dirname + dir;
+    form.keepExtensions = true;
+    form.maxFieldsSize = 10 * 1024 * 1024;
+    form.maxFields = 1000;
+    form.multiples = false;
+
+    form.parse(request, function(err, fields, files) {
+        var file = util.inspect(files);
+
+        response.writeHead(200, getHeaders('Content-Type', 'application/json'));
+
+        var fileName = file.split('path:')[1].split('\',')[0].split(dir)[1].toString().replace(/\\/g, '').replace(/\//g, '');
+        var fileURL = 'http://' + app.address + ':' + port + '/uploads/' + fileName;
+
+        console.log('fileURL: ', fileURL);
+        response.write(JSON.stringify({
+            fileURL: fileURL
+        }));
+        response.end();
+    });
+}
+
 module.exports = router;
